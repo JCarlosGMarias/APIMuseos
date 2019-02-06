af52ff8fc94012e05c39f112f0c0fab89bc646d4
JDev <jcarlosgmarias@gmail.com>
2019-02-06 23:49:36 +0100

    Ejercicios LINQ resueltos
    Ejercicios 2, 4 y 5 resueltos.

--- APIMuseos/APIMuseos/Program.cs
+++ APIMuseos/APIMuseos/Program.cs
@@ -1,6 +1,5 @@
 ﻿using APIMuseos.Models;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,9 +9,9 @@ using System.Threading.Tasks;
 namespace APIMuseos
 {
 
-//    https://museowebapp.azurewebsites.net/api/MuseosAPI
+    //    https://museowebapp.azurewebsites.net/api/MuseosAPI
 
-//https://www.newtonsoft.com/json/help/html/QueryingLINQtoJSON.htm
+    //https://www.newtonsoft.com/json/help/html/QueryingLINQtoJSON.htm
     class Museo /*Contenido de la API https://museowebapp.azurewebsites.net/api/MuseosAPI */
     {
         public int Id { get; set; }
@@ -27,74 +26,66 @@ namespace APIMuseos
     {
         static void Main(string[] args)
         {
-            //leeMuseos();
+            using (var Client = new HttpClient())
+            {
+                //leeMuseos(Client);
 
-            PhotosEx();
+                PhotosEx(Client);
 
-            Console.ReadLine();
+                Console.ReadLine();
+            }
         }
 
-        static async void leeMuseos()
+        static async void leeMuseos(HttpClient Client)
         {
             List<Museo> ListaMuseos = new List<Museo>();
 
-            using (var Clientes = new HttpClient())
+            using (var response = await Client.GetAsync(new Uri(@"https://museowebapp.azurewebsites.net/api/MuseosAPI")))
             {
-                var uri = new Uri(@"https://museowebapp.azurewebsites.net/api/MuseosAPI");
-
-                using (var response = await Clientes.GetAsync(uri))
+                if (response.IsSuccessStatusCode)
                 {
-                    if (response.IsSuccessStatusCode)
-                    {
-                        var content = await response.Content.ReadAsStringAsync();
-                        ListaMuseos = JsonConvert.DeserializeObject<List<Museo>>(content);
+                    var content = await response.Content.ReadAsStringAsync();
+                    ListaMuseos = JsonConvert.DeserializeObject<List<Museo>>(content);
 
-                        foreach (var mus in ListaMuseos)
-                        {
-                            Console.WriteLine(mus.Descripcion);
-                        }
+                    foreach (var mus in ListaMuseos)
+                    {
+                        Console.WriteLine(mus.Descripcion);
                     }
                 }
             }
         }
 
-        static async void leeMuseosVisitas()
+        static async void leeMuseosVisitas(HttpClient Client)
         {
-            List<Museo> ListaMuseos = new List<Museo>();
+            var ListaMuseos = new List<Museo>();
 
-            using (HttpClient Clientes = new HttpClient())
+            using (var response = await Client.GetAsync(new Uri("https://museowebapp.azurewebsites.net/api/MuseosAPI")))
             {
-                string url = @"https://museowebapp.azurewebsites.net/api/MuseosAPI";
-                var uri = new Uri(url);
-
-                using (var response = await Clientes.GetAsync(uri))
+                if (response.IsSuccessStatusCode)
                 {
-                    if (response.IsSuccessStatusCode)
-                    {
-                        var content = await response.Content.ReadAsStringAsync();
-                        //JArray Array = JArray.Parse(content);
-                        //var visitasS = from o in Array
-                        //               where p => p["visitas"] == "S"
-                        //               select o;
-                    }
+                    var content = await response.Content.ReadAsStringAsync();
+                    //JArray Array = JArray.Parse(content);
+                    //var visitasS = from o in Array
+                    //               where p => p["visitas"] == "S"
+                    //               select o;
                 }
             }
         }
 
-        static async void PhotosEx()
+        static async void PhotosEx(HttpClient Client)
         {
             // Usando jsonplaceholder.typicode.com
             var Root = new Uri(@"https://jsonplaceholder.typicode.com");
 
             // Crear las colecciones con los datos.
-            var Posts = await FetchTo<Post>($"{Root}posts");
-            var Comments = await FetchTo<Comment>($"{Root}comments");
-            var Albums = await FetchTo<Album>($"{Root}albums");
-            var Photos = await FetchTo<Photo>($"{Root}photos");
-            var Todos = await FetchTo<Todo>($"{Root}todos");
-            var Users = await FetchTo<User>($"{Root}users");
+            var Posts = await FetchTo<Post>(Client, $"{Root}posts");
+            var Comments = await FetchTo<Comment>(Client, $"{Root}comments");
+            var Albums = await FetchTo<Album>(Client, $"{Root}albums");
+            var Photos = await FetchTo<Photo>(Client, $"{Root}photos");
+            var Todos = await FetchTo<Todo>(Client, $"{Root}todos");
+            var Users = await FetchTo<User>(Client, $"{Root}users");
 
-            // Ver cuáles son los comentarios que hay para un post determinado
+            // 1: Ver cuáles son los comentarios que hay para un post determinado
             var PostID = 1;
 
             var CommentsForSinglePost = from c in Comments
@@ -108,26 +99,21 @@ namespace APIMuseos
             }
             Console.WriteLine("");
 
-            // Ver cuáles son los álbums que hay y la cantidad de fotos que contiene cada uno de ellos
+            // 2: Ver cuáles son los álbums que hay y la cantidad de fotos que contiene cada uno de ellos
             var PhotosInAlbums = from p in Photos
                                  join a in Albums on p.albumId equals a.id
                                  group p by a.title into photos
                                  orderby photos.Key
-                                 select photos;
+                                 select new { Title = photos.Key, Total = photos.Count() };
 
-            Console.WriteLine($"Comments for post {PostID}:{Environment.NewLine}");
+            Console.WriteLine($"Photos in each album:");
             foreach (var Album in PhotosInAlbums)
             {
-                Console.WriteLine($"- {Album.Key}:");
-                foreach (var Photo in Album)
-                {
-                    Console.WriteLine($"  * {Photo.title}: {Photo.thumbnailUrl}");
-
-                }
+                Console.WriteLine($"- {Album.Title}: {Album.Total}");
             }
             Console.WriteLine("");
 
-            // Ver cuáles son los nombres de los usuarios ordenados descendentemente
+            // 3: Ver cuáles son los nombres de los usuarios ordenados descendentemente
             var UserNames = from u in Users
                             orderby u.name descending
                             select u.name;
@@ -139,37 +125,62 @@ namespace APIMuseos
             }
             Console.WriteLine("");
 
-            // Tomar los cinco álbums donde más fotos existan
+            // 4: Tomar los cinco álbums donde más fotos existan
+            var Top5MostPopulatedAlbums = (from a in Albums
+                                           join p in Photos on a.id equals p.albumId
+                                           group p by a.title into photos
+                                           orderby photos.Count(), photos.Key
+                                           select new { Title = photos.Key, Total = photos.Count() })
+                                          .Take(5);
+
+            Console.WriteLine($"Photos in each album:");
+            foreach (var Album in Top5MostPopulatedAlbums)
+            {
+                Console.WriteLine($"- {Album.Title}: {Album.Total}");
+            }
+            Console.WriteLine("");
 
 
-            // Ver qué cantidad de comentarios totales ha recibido un usuario
+            // 5: Ver qué cantidad de comentarios totales ha recibido un usuario
+            var UserID = 4;
+
+            var CommentsPerUser = from u in Users
+                                  join p in Posts on u.id equals p.userId
+                                  join c in Comments on p.id equals c.postId
+                                  where u.id == UserID
+                                  group c by u.name into comments
+                                  orderby comments.Count(), comments.Key
+                                  select new { UserName = comments.Key, TotalComments = comments.Count() };
+
+            Console.WriteLine($"Comments per user:");
+            foreach (var User in CommentsPerUser)
+            {
+                Console.WriteLine($"- {User.UserName} - Comments = {User.TotalComments}");
+            }
+            Console.WriteLine("");
+
+            // 6: Mostrar el usuario que más comentarios ha recibido
 
 
-            // Mostrar el usuario que más comentarios ha recibido
+            // 7: Mostrar para cada usuario cuántas tareas tiene terminadas y cuáles no
 
 
-            // Mostrar para cada usuario cuántas tareas tiene terminadas y cuáles no
+            // 8: Mostrar cuál es el usuario que más tareas tiene por finalizar
 
 
-            // Mostrar cuál es el usuario que más tareas tiene por finalizar
-
-
-            // Mostrar todas las urls de las imágenes del usuario que más fotos ha subido
+            // 9: Mostrar todas las urls de las imágenes del usuario que más fotos ha subido
         }
 
-        static async Task<List<T>> FetchTo<T>(string Resource) where T : class
+        static async Task<List<T>> FetchTo<T>(HttpClient Client, string Resource) where T : class
         {
             List<T> Result = new List<T>();
 
-            using (var Client = new HttpClient())
+            using (var response = await Client.GetAsync(new Uri(Resource)))
             {
-                using (var response = await Client.GetAsync(new Uri(Resource)))
+                if (response.IsSuccessStatusCode)
                 {
-                    if (response.IsSuccessStatusCode)
-                    {
-                        var content = await response.Content.ReadAsStringAsync();
-                        Result = JsonConvert.DeserializeObject<List<T>>(content);
-                    }
+                    var content = await response.Content.ReadAsStringAsync();
+                    Result = JsonConvert.DeserializeObject<List<T>>(content);
                 }
             }
 
