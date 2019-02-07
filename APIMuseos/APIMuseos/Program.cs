21c261f897e69c2af3bafdb56f9e6561f311d9d8
JDev <jcarlosgmarias@gmail.com>
2019-02-07 13:32:30 +0100

    Todos los ejercicios LINQ resueltos

--- APIMuseos/APIMuseos/Program.cs
+++ APIMuseos/APIMuseos/Program.cs
@@ -15,188 +15,214 @@ namespace APIMuseos
     {
         static void Main(string[] args)
         {
-            using (var Client = new HttpClient())
-            {
-                //leeMuseos(Client);
-
-                PhotosEx(Client);
-            }
+            //leeMuseos();
+            //leeMuseosVisitas();
+            PhotosEx();
 
             Console.ReadLine();
         }
 
-        static async void leeMuseos(HttpClient Client)
+        static async void leeMuseos()
         {
-            List<Museo> ListaMuseos = new List<Museo>();
-
-            using (var response = await Client.GetAsync(new Uri(@"https://museowebapp.azurewebsites.net/api/MuseosAPI")))
+            using (var Client = new HttpClient())
             {
-                if (response.IsSuccessStatusCode)
-                {
-                    var content = await response.Content.ReadAsStringAsync();
-                    ListaMuseos = JsonConvert.DeserializeObject<List<Museo>>(content);
+                List<Museo> ListaMuseos = new List<Museo>();
 
-                    foreach (var mus in ListaMuseos)
+                using (var response = await Client.GetAsync(new Uri(@"https://museowebapp.azurewebsites.net/api/MuseosAPI")))
+                {
+                    if (response.IsSuccessStatusCode)
                     {
-                        Console.WriteLine(mus.Descripcion);
+                        var content = await response.Content.ReadAsStringAsync();
+                        ListaMuseos = JsonConvert.DeserializeObject<List<Museo>>(content);
+
+                        foreach (var mus in ListaMuseos)
+                        {
+                            Console.WriteLine(mus.Descripcion);
+                        }
                     }
                 }
             }
         }
 
-        static async void leeMuseosVisitas(HttpClient Client)
+        static async void leeMuseosVisitas()
         {
-            var ListaMuseos = new List<Museo>();
-
-            using (var response = await Client.GetAsync(new Uri("https://museowebapp.azurewebsites.net/api/MuseosAPI")))
+            using (var Client = new HttpClient())
             {
-                if (response.IsSuccessStatusCode)
+                var ListaMuseos = new List<Museo>();
+
+                using (var response = await Client.GetAsync(new Uri("https://museowebapp.azurewebsites.net/api/MuseosAPI")))
                 {
-                    var content = await response.Content.ReadAsStringAsync();
-                    //JArray Array = JArray.Parse(content);
-                    //var visitasS = from o in Array
-                    //               where p => p["visitas"] == "S"
-                    //               select o;
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var content = await response.Content.ReadAsStringAsync();
+                        //JArray Array = JArray.Parse(content);
+                        //var visitasS = from o in Array
+                        //               where p => p["visitas"] == "S"
+                        //               select o;
+                    }
                 }
             }
         }
 
-        static async void PhotosEx(HttpClient Client)
+        static async void PhotosEx()
         {
-            // Usando jsonplaceholder.typicode.com
-            
-            var Root = new Uri(@"https://jsonplaceholder.typicode.com");
-            Console.WriteLine($"API URL -> {Root}. Fetching resources...");
-
-            // Crear las colecciones con los datos.
-            var Posts = await FetchTo<Post>(Client, $"{Root}posts");
-            var Comments = await FetchTo<Comment>(Client, $"{Root}comments");
-            var Albums = await FetchTo<Album>(Client, $"{Root}albums");
-            var Photos = await FetchTo<Photo>(Client, $"{Root}photos");
-            var Todos = await FetchTo<Todo>(Client, $"{Root}todos");
-            var Users = await FetchTo<User>(Client, $"{Root}users");
-
-            Console.WriteLine($"Starting LINQ queries...{Environment.NewLine}");
-
-            #region 1: Ver cuáles son los comentarios que hay para un post determinado
-            var PostID = 1;
-
-            var CommentsForSinglePost = from c in Comments
-                                        where c.postId == (from p in Posts where p.id == PostID select p).First().id
-                                        select c;
-
-            Console.WriteLine($"Comments for post {PostID}:{Environment.NewLine}");
-            foreach (var Comment in CommentsForSinglePost)
+            using (var Client = new HttpClient())
             {
-                Console.WriteLine($"- {Comment.name} ({Comment.email}) -> {Comment.body}");
+                // Usando jsonplaceholder.typicode.com
+
+                var Root = new Uri(@"https://jsonplaceholder.typicode.com");
+                Console.WriteLine($"API URL -> {Root}. Fetching resources...");
+
+                // Crear las colecciones con los datos.
+                var Posts = await FetchTo<Post>(Client, $"{Root}posts");
+                var Comments = await FetchTo<Comment>(Client, $"{Root}comments");
+                var Albums = await FetchTo<Album>(Client, $"{Root}albums");
+                var Photos = await FetchTo<Photo>(Client, $"{Root}photos");
+                var Todos = await FetchTo<Todo>(Client, $"{Root}todos");
+                var Users = await FetchTo<User>(Client, $"{Root}users");
+
+                Console.WriteLine($"Starting LINQ queries...{Environment.NewLine}");
+
+                #region 1: Ver cuáles son los comentarios que hay para un post determinado
+                var PostID = 1;
+
+                var CommentsForSinglePost = from c in Comments
+                                            where c.postId == (from p in Posts where p.id == PostID select p.id).FirstOrDefault()
+                                            select c;
+
+                Console.WriteLine($"Comments for post {PostID}:{Environment.NewLine}");
+                foreach (var Comment in CommentsForSinglePost)
+                {
+                    Console.WriteLine($"- {Comment.name} ({Comment.email}) -> {Comment.body}");
+                }
+                Console.WriteLine("");
+                #endregion
+
+                #region 2: Ver cuáles son los álbums que hay y la cantidad de fotos que contiene cada uno de ellos
+                var PhotosInAlbums = from p in Photos
+                                     join a in Albums on p.albumId equals a.id
+                                     group p by a.title into photos
+                                     orderby photos.Key
+                                     select new { Title = photos.Key, Total = photos.Count() };
+
+                Console.WriteLine($"Photos in each album:");
+                foreach (var Album in PhotosInAlbums)
+                {
+                    Console.WriteLine($"- {Album.Title}: {Album.Total}");
+                }
+                Console.WriteLine("");
+                #endregion
+
+                #region 3: Ver cuáles son los nombres de los usuarios ordenados descendentemente
+                var UserNames = from u in Users
+                                orderby u.name descending
+                                select u.name;
+
+                Console.WriteLine($"Sorted down user names:");
+                foreach (var UserName in UserNames)
+                {
+                    Console.WriteLine($"- {UserName}");
+                }
+                Console.WriteLine("");
+                #endregion
+
+                #region 4: Tomar los cinco álbums donde más fotos existan
+                var Top5MostPopulatedAlbums = (from a in Albums
+                                               join p in Photos on a.id equals p.albumId
+                                               group p by a.title into photos
+                                               orderby photos.Count(), photos.Key
+                                               select new { Title = photos.Key, Total = photos.Count() })
+                                              .Take(5);
+
+                Console.WriteLine($"Top 5 Most Populated albums:");
+                foreach (var Album in Top5MostPopulatedAlbums)
+                {
+                    Console.WriteLine($"- {Album.Title}: {Album.Total}");
+                }
+                Console.WriteLine("");
+                #endregion
+
+                #region 5: Ver qué cantidad de comentarios totales ha recibido un usuario
+                var UserID = 3;
+
+                var CommentsPerUser = from u in Users
+                                      join p in Posts on u.id equals p.userId
+                                      join c in Comments on p.id equals c.postId
+                                      where u.id == UserID
+                                      group c by u.name into comments
+                                      orderby comments.Count(), comments.Key
+                                      select new { UserName = comments.Key, TotalComments = comments.Count() };
+
+                Console.WriteLine($"Comments per user:");
+                foreach (var User in CommentsPerUser)
+                {
+                    Console.WriteLine($"- {User.UserName} - Comments = {User.TotalComments}");
+                }
+                Console.WriteLine("");
+                #endregion
+
+                #region 6: Mostrar el usuario que más comentarios ha recibido
+                var MostCommentedUser = (from u in Users
+                                         join p in Posts on u.id equals p.userId
+                                         join c in Comments on p.id equals c.postId
+                                         group c by u.name into comments
+                                         orderby comments.Count() descending
+                                         select new { UserName = comments.Key, Total = comments.Count() }).First();
+
+                Console.WriteLine($"Most commented user:");
+                Console.WriteLine($"- {MostCommentedUser.UserName} - Comments = {MostCommentedUser.Total}{Environment.NewLine}{Environment.NewLine}");
+                #endregion
+
+                #region 7: Mostrar para cada usuario cuántas tareas tiene terminadas y cuáles no
+                var TaskStatusesPerUser = from u in Users
+                                          select new
+                                          {
+                                              UserName = u.name,
+                                              //FinishedTasksCount = (from us in Users
+                                              //                      join t in Todos on us.id equals t.userId)
+                                              FinishedTasksCount = (from t in Todos where t.userId == u.id && t.completed select t).Count(),
+                                              UnfinishedTasks = from t in Todos where t.userId == u.id && !t.completed select t
+                                          };
+
+                Console.WriteLine($"Todos status per user:");
+                foreach (var User in TaskStatusesPerUser)
+                {
+                    Console.WriteLine($"- User: {User.UserName} (Finished todos: {User.FinishedTasksCount}) (Remaining todos: {User.UnfinishedTasks.Count()})");
+                    foreach (var Todo in User.UnfinishedTasks)
+                    {
+                        Console.WriteLine($"  * Todo: {Todo.title}");
+                    }
+                }
+                Console.WriteLine($"{Environment.NewLine}{Environment.NewLine}");
+                #endregion
+
+                #region 8: Mostrar cuál es el usuario que más tareas tiene por finalizar
+                var MostDelayedUser = (from u in Users
+                                       join t in Todos on u.id equals t.userId
+                                       group t by u.name into users
+                                       orderby users.Count() descending
+                                       select new { UserName = users.Key, Total = users.Count() }).First();
+
+                Console.WriteLine($"Most delayed user:");
+                Console.WriteLine($"- {MostDelayedUser.UserName} - Todos = {MostDelayedUser.Total}{Environment.NewLine}{Environment.NewLine}");
+                #endregion
+
+                #region 9: Mostrar todas las urls de las imágenes del usuario que más fotos ha subido
+                var ImagesFromMostCollaborativeUser = (from u in Users
+                                                       join a in Albums on u.id equals a.userId
+                                                       join p in Photos on a.id equals p.albumId
+                                                       group p by u.name into photos
+                                                       orderby photos.Count() descending
+                                                       select new { UserName = photos.Key, Total = photos.Count(), Photos = photos.ToList() }).First();
+
+                Console.WriteLine($"Photos from most collaborative user:");
+                Console.WriteLine($"- User: {ImagesFromMostCollaborativeUser.UserName} (Total photos: {ImagesFromMostCollaborativeUser.Total})");
+                foreach (var Photo in ImagesFromMostCollaborativeUser.Photos)
+                {
+                    Console.WriteLine($"  * Url: {Photo.url}");
+                }
+                #endregion
             }
-            Console.WriteLine("");
-            #endregion
-
-            #region 2: Ver cuáles son los álbums que hay y la cantidad de fotos que contiene cada uno de ellos
-            var PhotosInAlbums = from p in Photos
-                                 join a in Albums on p.albumId equals a.id
-                                 group p by a.title into photos
-                                 orderby photos.Key
-                                 select new { Title = photos.Key, Total = photos.Count() };
-
-            Console.WriteLine($"Photos in each album:");
-            foreach (var Album in PhotosInAlbums)
-            {
-                Console.WriteLine($"- {Album.Title}: {Album.Total}");
-            }
-            Console.WriteLine("");
-            #endregion
-
-            #region 3: Ver cuáles son los nombres de los usuarios ordenados descendentemente
-            var UserNames = from u in Users
-                            orderby u.name descending
-                            select u.name;
-
-            Console.WriteLine($"Sorted down user names:");
-            foreach (var UserName in UserNames)
-            {
-                Console.WriteLine($"- {UserName}");
-            }
-            Console.WriteLine("");
-            #endregion
-
-            #region 4: Tomar los cinco álbums donde más fotos existan
-            var Top5MostPopulatedAlbums = (from a in Albums
-                                           join p in Photos on a.id equals p.albumId
-                                           group p by a.title into photos
-                                           orderby photos.Count(), photos.Key
-                                           select new { Title = photos.Key, Total = photos.Count() })
-                                          .Take(5);
-
-            Console.WriteLine($"Top 5 Most Populated albums:");
-            foreach (var Album in Top5MostPopulatedAlbums)
-            {
-                Console.WriteLine($"- {Album.Title}: {Album.Total}");
-            }
-            Console.WriteLine("");
-            #endregion
-
-            #region 5: Ver qué cantidad de comentarios totales ha recibido un usuario
-            var UserID = 3;
-
-            var CommentsPerUser = from u in Users
-                                  join p in Posts on u.id equals p.userId
-                                  join c in Comments on p.id equals c.postId
-                                  where u.id == UserID
-                                  group c by u.name into comments
-                                  orderby comments.Count(), comments.Key
-                                  select new { UserName = comments.Key, TotalComments = comments.Count() };
-
-            Console.WriteLine($"Comments per user:");
-            foreach (var User in CommentsPerUser)
-            {
-                Console.WriteLine($"- {User.UserName} - Comments = {User.TotalComments}");
-            }
-            Console.WriteLine("");
-            #endregion
-
-            #region 6: Mostrar el usuario que más comentarios ha recibido
-            var MostCommentedUser = (from u in Users
-                                    join p in Posts on u.id equals p.userId
-                                    join c in Comments on p.id equals c.postId
-                                    group c by u.name into comments
-                                    orderby comments.Count() descending
-                                    select new { UserName = comments.Key, Total = comments.Count() }).First();
-
-            Console.WriteLine($"Most commented user:");
-            Console.WriteLine($"- {MostCommentedUser.UserName} - Comments = {MostCommentedUser.Total}{Environment.NewLine}{Environment.NewLine}");
-            #endregion
-
-            #region 7: Mostrar para cada usuario cuántas tareas tiene terminadas y cuáles no
-            #endregion
-
-            #region 8: Mostrar cuál es el usuario que más tareas tiene por finalizar
-            var MostDelayedUser = (from u in Users
-                                   join t in Todos on u.id equals t.userId
-                                   group t by u.name into users
-                                   orderby users.Count() descending
-                                   select new { UserName = users.Key, Total = users.Count() }).First();
-
-            Console.WriteLine($"Most delayed user:");
-            Console.WriteLine($"- {MostDelayedUser.UserName} - Todos = {MostDelayedUser.Total}{Environment.NewLine}{Environment.NewLine}");
-            #endregion
-
-            #region 9: Mostrar todas las urls de las imágenes del usuario que más fotos ha subido
-            var ImagesFromMostCollaborativeUser = (from u in Users
-                                                   join a in Albums on u.id equals a.userId
-                                                   join p in Photos on a.id equals p.albumId
-                                                   group p by u.name into photos
-                                                   orderby photos.Count() descending
-                                                   select new { UserName = photos.Key, Total = photos.Count(), Photos = photos.ToList() }).First();
-
-            Console.WriteLine($"Photos from most collaborative user:");
-            Console.WriteLine($"- User: {ImagesFromMostCollaborativeUser.UserName} (Total photos: {ImagesFromMostCollaborativeUser.Total})");
-            foreach (var Photo in ImagesFromMostCollaborativeUser.Photos)
-            {
-                Console.WriteLine($"  * Url: {Photo.url}");
-            }
-            #endregion
         }
 
         static async Task<List<T>> FetchTo<T>(HttpClient Client, string Resource) where T : class
